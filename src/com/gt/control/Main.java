bbd025e6e823624f72ddf0ebdc994dbe6420b190
7eo Aldamas <leo.aldamas@gmail.com>
2021-10-13 22:25:46 -0600

    Inicio Git

---
+++ src/com/gt/control/Main.java
@@ -0,0 +1,11 @@
+package com.gt.control;
+
+public class Main {
+//---
+    
+    public static void main(String[] args) {
+        //--- Código Ejecutable
+        System.out.println("Hola Git");
+    }
+    
+}
